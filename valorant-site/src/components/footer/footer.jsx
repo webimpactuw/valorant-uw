66029c65f2288988b4c7258e51adbb05eb28fe25
Kevin Nguyen <kevinn24@uw.edu>
2025-04-08 22:26:45 -0700

    simple footer
    simple setup footer

---
+++ valorant-site/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css'
+
+function Footer(){ 
+    return (
+        <footer className='footer'>
+            <div className='footer-container'>
+                <p className='copyright'>COPYRIGHT @2025 VALORANT AT UW</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer 
