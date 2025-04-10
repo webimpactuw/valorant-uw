74c9bd28dcc87b7a334578e20aa2ef217135ef38
celestinebuendia <celestinembuendia@yahoo.com>
2025-04-09 22:41:24 -0700

    Footer styling

--- valorant-site/src/components/footer/footer.jsx
+++ valorant-site/src/components/footer/footer.jsx
@@ -4,9 +4,7 @@ import './footer.css'
 function Footer(){ 
     return (
         <footer className='footer'>
-            <div className='footer-container'>
-                <p className='copyright'>COPYRIGHT @2025 VALORANT AT UW</p>
-            </div>
+            <p className='copyright'>COPYRIGHT @2025 VALORANT AT UW</p>
         </footer>
     );
 }
