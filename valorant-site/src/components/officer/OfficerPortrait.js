cd73748fb4842da607aa0fc47bbed951ff6485e6
Quashnock <awq@uw.edu>
2025-05-04 15:38:57 -0700

    Added Kim to events page, added DINish regular font weight and used it in events, renamed operators_portraits folder to operator_portraits, and improved SEO with titles, alt tags, and better semantic HTML.

--- valorant-site/src/components/officer/OfficerPortrait.js
+++ valorant-site/src/components/officer/OfficerPortrait.js
@@ -1,15 +1,24 @@
-function OfficerPortrait({image, operatorImage, name, role, gradYear}) {
+function OfficerPortrait({image, operatorImage, name, operatorName, role, gradYear}) {
     return ( 
         <div className = "w-full flex flex-col gap-[21px]">
-            <div className= "w-full h-auto relative">
-                <img src = {image} className = "w-full h-auto border-[3px] border-[#9886d0] object-cover aspect-[4/5] "/>
-                <img src = {operatorImage} className = "absolute bottom-[-8%] right-[-8%] w-[30%] rounded-full z-[2]"/>
-            </div>
-            <div className = "text-left text-2xl uppercase font-dinish">
+            <figure className= "w-full h-auto relative">
+                <img 
+                    src = {image} 
+                    alt = {`Portrait of ${name}`}
+                    className = "w-full h-auto border-[3px] border-[#9886d0] object-cover aspect-[4/5] "
+                />
+                <img 
+                    src = {operatorImage} 
+                    alt = {`Favorite operator: ${operatorName}`} 
+                    title = {`Favorite Operator: ${operatorName}`} 
+                    className = "absolute bottom-[-8%] right-[-8%] w-[30%] rounded-full z-[2]"
+                />
+            </figure>
+            <figcaption className = "text-left text-2xl uppercase font-dinish">
                 <h4 className = "font-black">{name}</h4>
-                <h5 className = "font-normal">{role}</h5>
+                <h5 className = "font-normal text-opacity-[1]">{role}</h5>
                 <h5 className = "font-normal">Class of {gradYear}</h5>
-            </div>
+            </figcaption>
         </div>
     );
 }
