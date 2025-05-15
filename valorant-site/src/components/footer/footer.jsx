77f70eaf77760bddc5ce61f756f7308a547825f8
Kevin Nguyen <kevinn24@uw.edu>
2025-05-14 22:10:29 -0700

    add bar icon on header for mobile + fix footer on mobile

--- valorant-site/src/components/footer/footer.jsx
+++ valorant-site/src/components/footer/footer.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import './footer.css'
 import logo from '../../assets/valorant_at_uw_logo.svg';
 import { Link } from 'react-router-dom';
 
 function Footer(){ 
     return (
-        <footer className='footer'>
-            <div><Link to={"/"}><img src={logo} alt='Logo' height={55} width={55}></img></Link></div>
-            <p className='copyright'>COPYRIGHT © 2025 VALORANT AT UW</p>
+        <footer className='bg-[#4C0080] text-white w-full h-16 px-4 py-2.5 flex justify-between items-center overflow-hidden'>
+            <div><Link to={"/"}><img className="w-12 h-12 rounded" src={logo} alt='Logo' height={55} width={55}></img></Link></div>
+            <p className="text-nowrap justify-start text-white text-sm md:text-2xl font-bold md:font-extrabold font-['DINish'] uppercase">COPYRIGHT © 2025 VALORANT AT UW</p>
         </footer>
     );
 }
--- valorant-site/src/components/header/Header.js
+++ valorant-site/src/components/header/Header.js
@@ -4,11 +4,20 @@ import logo from '../../assets/valorant_at_uw_logo.svg';
 function Header() {
     return (
         <div className="w-full h-16 relative bg-[#4C0080] overflow-hidden flex items-center justify-between px-14">
-            <Link to="/"><img className="w-12 h-center left-[0.875rem] top-[0.5rem] absolute rounded-[51px]" src={logo}/></Link>
+            <Link to="/"><img className="w-12 h-center left-[0.875rem] top-[0.5rem] absolute rounded-[51px]" src={logo} alt='Logo'/></Link>
             <div className="flex justify-end items-center gap-14 px-4">
-                <div className="justify-start text-white text-2xl font-extrabold font-['DINish'] uppercase"><Link to="/">HOME</Link></div>
-                <div className="justify-start text-white text-2xl font-extrabold font-['DINish'] uppercase"><Link to="/events">EVENTS</Link></div>
-                <div className="justify-start text-white text-2xl font-extrabold font-['DINish'] uppercase"><Link to="/officers">OFFICERS</Link></div>
+                <div className="justify-start text-white text-2xl font-extrabold font-['DINish'] uppercase"><Link to="/">
+                <span className="hidden sm:inline">HOME</span></Link></div>
+                <div className="justify-start text-white text-2xl font-extrabold font-['DINish'] uppercase"><Link to="/events">
+                <span className="hidden sm:inline">EVENTS</span></Link></div>
+                <div className="justify-start text-white text-2xl font-extrabold font-['DINish'] uppercase"><Link to="/officers">
+                <span className="hidden sm:inline">OFFICERS</span></Link></div>
+            </div>
+            <div className="w-8 h-8 relative sm:hidden overflow-hidden">
+                <div className="w-8 h-8 left-0 top-0 absolute"></div>
+                <div className="w-6 h-0 left-[5.67px] top-[8.50px] absolute outline outline-2 outline-offset-[-1px] outline-white"></div>
+                <div className="w-6 h-0 left-[5.67px] top-[17px] absolute outline outline-2 outline-offset-[-1px] outline-white"></div>
+                <div className="w-6 h-0 left-[5.67px] top-[25.50px] absolute outline outline-2 outline-offset-[-1px] outline-white"></div>
             </div>
         </div>
     );
